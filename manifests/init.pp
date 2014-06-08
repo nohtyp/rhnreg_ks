0cc721df06e7f0f8362ade04668a1ff36291be1a
Thomas Foster <thomas.foster80@gmail.com>
2014-06-08 11:50:36 -0400

    added init.pp and params.pp files

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,61 @@
-class rhnreg_ks {
+class rhnreg_ks (
+  $serverurl = 'satellite serverurl',
+  $username = 'root',
+  $password = 'password',
+  $server = $::hostname,
+  $profilename = $::fqdn,
+  $path = '/etc/sysconfig/rhn/up2date',
+  $myup2date = 'serverURL=https://*',
+  $replaceurl = 'serverURL=https://vasat.promnetwork.com/XMLRPC',
+  $presentorabsent = 'present',
+  $cacert = '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
+  $useforce = false,
+  $activationkeys = $rhnreg_ks::params::activationkeys
+) inherits rhnreg_ks::params {
+
+  if $activationkeys == 'undef' {
+    fail('No Activation key specified found for system')
+  }
+
+  rhn_register { $server:
+  ensure         => $presentorabsent,
+  activationkeys => $activationkeys,
+  username       => $username,
+  password       => $password,
+  profile_name   => $profilename,
+  server_url     => $serverurl,
+  ssl_ca_cert    => $cacert,
+  force          => $useforce,
+  }
+
+  file_line { $::fqdn:
+    path    => $path,
+    match   => $myup2date,
+    line    => $replaceurl,
+    require => Rhn_register["$server"],
+  }
+
+  package {'rhn-client-tools':
+    ensure => 'installed',
+  }
+
+  package {'rhncfg-actions':
+    ensure => 'installed',
+  }
+
+  package {'rhnsd':
+    ensure => 'installed',
+  }
+
+  service {'rhnsd':
+    ensure => 'running',
+  }
+
+  exec { 'rhn-actions-control':
+    path    => '/usr/bin/',
+    command => 'rhn-actions-control --enable-all',
+    onlyif  => 'rhn-actions-control --report | /bin/grep disabled',
+    require => Package ['rhncfg-actions'],
+  }
 }
+
